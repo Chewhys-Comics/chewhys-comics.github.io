0d2eebb50e96d73a89c648534e89d504d4cebdcd
StephY0728 <3011316681@qq.com>
2024-03-28 00:44:09 +0800

    Update, #2f28c6

---
+++ js/footer_contact.js
@@ -0,0 +1,12 @@
+/*************************************************************************
+ * For loading the footer for contact page.
+ * The footer will not work if you open the file directly rather than on a web server. xampp is okay tho lol
+ *************************************************************************/
+
+(function () {
+    "use strict";
+  
+    $(function () {
+      $(".contact-footer-container").load("contact-footer.html");
+    });
+  })();
